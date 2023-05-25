409abd8e927698183e8a9501bf036c244184654a
danii <daniilkimstachp@gmail.com>
2023-05-25 21:00:31 +0300

    lab was updated

--- solving/minimizing_karno.py
+++ solving/minimizing_karno.py
@@ -64,8 +64,7 @@ class Karnough:
         second_value = args[1]
         third_value = args[2]
         res = []
-        for _ in range(3):
-            res.append([0, 0, 0, 0, 0])
+        for _ in range(3): res.append([0, 0, 0, 0, 0])
         action = self.actions
         res[1][0] = f'!{first_value}'
         res[2][0] = first_value
@@ -90,51 +89,42 @@ class Karnough:
         return values, existing
 
     def check_figure(self, res, vertical_value, index, g_index, existing) -> bool:
-        if index == 4:
-            return False
-        if vertical_value != self.CONSTANT_ONE:
-            return False
-        if res[g_index][index + 1] != self.CONSTANT_ONE:
-            return False
-        if g_index == 2 or res[g_index + 1][index] != self.CONSTANT_ONE:
-            return False
-        if res[g_index + 1][index + 1] != self.CONSTANT_ONE:
-            return False
+        if index == 4: return False
+        if vertical_value != self.CONSTANT_ONE: return False
+        if res[g_index][index + 1] != self.CONSTANT_ONE: return False
+        if g_index == 2 or res[g_index + 1][index] != self.CONSTANT_ONE: return False
+        if res[g_index + 1][index + 1] != self.CONSTANT_ONE: return False
         cells = [[g_index, index], [g_index + 1, index + 1], [g_index + 1, index], [g_index, index + 1]]
-        if self.checking_items(cells, existing):
-            return False
+        if self.checking_items(cells, existing): return False
         return True
 
     def figure_side(self, res, vertical, index, g_index, existing) -> bool:
-        if g_index == 2:
-            return False
-        if index != 1 or vertical != self.CONSTANT_ONE:
-            return False
-        if res[g_index + 1][index] != self.CONSTANT_ONE:
-            return False
-        if res[g_index][4] != self.CONSTANT_ONE or res[g_index + 1][4] != self.CONSTANT_ONE:
-            return False
+        if g_index == 2: return False
+        if index != 1 or vertical != self.CONSTANT_ONE: return False
+        if res[g_index + 1][index] != self.CONSTANT_ONE: return False
+        if res[g_index][4] != self.CONSTANT_ONE or res[g_index + 1][4] != self.CONSTANT_ONE: return False
         cells = [[g_index, index], [g_index + 1, index], [g_index, 4], [g_index + 1, 4]]
-        if self.checking_items(cells, existing):
-            return False
+        if self.checking_items(cells, existing): return False
         return True
 
     @staticmethod
     def karno_figure(values, verticals, gorizontals, existing):
         values.append([verticals, verticals + 1, 0])
-        existing.extend([[gorizontals, verticals],
-                         [gorizontals + 1, verticals + 1],
-                         [gorizontals + 1, verticals],
-                         [gorizontals, verticals + 1]])
+        existing.extend([
+            [gorizontals, verticals],
+            [gorizontals + 1, verticals + 1],
+            [gorizontals + 1, verticals],
+            [gorizontals, verticals + 1]])
         return values, existing
 
     @staticmethod
     def second_karno_figure(neighbor_indexes, col_ind, row_ind, founded):
         neighbor_indexes.append([col_ind, 4, 0])
-        founded.extend([[row_ind, col_ind],
-                        [row_ind + 1, col_ind],
-                        [row_ind, 4],
-                        [row_ind + 1, 4]])
+        founded.extend([
+            [row_ind, col_ind],
+            [row_ind + 1, col_ind],
+            [row_ind, 4],
+            [row_ind + 1, 4]])
         return neighbor_indexes, founded
 
     def check_four_figure(self, res, values, existing):
@@ -159,19 +149,34 @@ class Karnough:
             for col_ind, col_value in enumerate(element):
                 if col_ind == 0:
                     continue
-                if index != 2 and col_value == self.CONSTANT_ONE and res[index + 1][col_ind] == self.CONSTANT_ONE \
-                        and ([index, col_ind] not in founded or [index + 1, col_ind] not in founded):
-                    neighbor_indexes.append([index, index + 1, col_ind, col_ind])
-                    founded.extend([[index, col_ind], [index + 1, col_ind]])
+                if index != 2 and col_value == self.CONSTANT_ONE and res[index + 1][col_ind] == self.CONSTANT_ONE and (
+                        [index, col_ind] not in founded or [index + 1, col_ind] not in founded):
+                    neighbor_indexes.append(
+                        [index, index + 1, col_ind, col_ind]), \
+                        founded.extend(
+                            [
+                                [index, col_ind],
+                                [index + 1, col_ind]
+                            ])
                 elif col_ind != 4 and col_value == self.CONSTANT_ONE and res[index][
-                    col_ind + 1] == self.CONSTANT_ONE \
-                        and ([index, col_ind] not in founded or [index, col_ind + 1] not in founded):
-                    neighbor_indexes.append([index, index, col_ind, col_ind + 1])
-                    founded.extend([[index, col_ind], [index, col_ind + 1]])
-                elif col_ind == 1 and col_value == self.CONSTANT_ONE and element[-1] == self.CONSTANT_ONE \
-                        and ([index, 4] not in founded or [index, col_ind] not in founded):
-                    neighbor_indexes.append([index, index, col_ind, -1])
-                    founded.extend([[index, col_ind], [index, 4]])
+                    col_ind + 1] == self.CONSTANT_ONE and (
+                        [index, col_ind] not in founded or [index, col_ind + 1] not in founded):
+                    neighbor_indexes.append(
+                        [index, index, col_ind, col_ind + 1]), \
+                        founded.extend(
+                            [
+                                [index, col_ind],
+                                [index, col_ind + 1]
+                            ])
+                elif col_ind == 1 and col_value == self.CONSTANT_ONE and element[-1] == self.CONSTANT_ONE and (
+                        [index, 4] not in founded or [index, col_ind] not in founded):
+                    neighbor_indexes.append(
+                        [index,
+                         index,
+                         col_ind, -1]), \
+                        founded.extend([
+                            [index, col_ind],
+                            [index, 4]])
         return neighbor_indexes, founded
 
     def one_line(self, res, values, existing):
@@ -182,18 +187,27 @@ class Karnough:
                 if column_index == 0:
                     continue
                 if column == self.CONSTANT_ONE and [index, column_index] not in existing:
-                    values.append([index, column_index])
-                    existing.append([index, column_index])
+                    values, existing = self.processing_single_line(index, column_index, existing, values)
+        return values, existing
+
+    @staticmethod
+    def processing_single_line(index, column_index, existing, values):
+        values.append([index, column_index])
+        existing.append([index, column_index])
         return values, existing
 
     def result(self, res):
         values = []
         existing = []
+        values, existing = self.getting_result(res, values, existing)
+        return self.sum(res, values)
+
+    def getting_result(self, res, values, existing):
         values, existing = self.check_line(res, values, existing)
         values, existing = self.check_four_figure(res, values, existing)
         values, existing = self.two_line(res, values, existing)
         values, existing = self.one_line(res, values, existing)
-        return self.sum(res, values)
+        return values, existing
 
     def get_four(self, table, item):
         first_value = table[item[0]][0] + self.actions + table[0][item[2]]
@@ -203,20 +217,17 @@ class Karnough:
 
     def get_two(self, res, element, flag):
         value = f'{res[element[0]][0]}{self.actions}{res[0][element[1]]}'
-        if flag:
-            value = self.actions.join(
-                [f'!{x}' if x.find('!') == -1 else x.replace('!', '') for x in
-                 value.split(self.actions)]
-            )
+        if flag: value = self.actions.join(
+            [f'!{x}' if x.find('!') == -1 else x.replace('!', '') for x in value.split(self.actions)])
         return value
 
     @staticmethod
     def check_values(res, element, flag):
         value = res[element[0]][0]
-        if flag:
-            value = value.replace('!', '') if '!' in value else f'!{value}'
-        else:
-            value = value if '!' in value else value.replace('!', '')
+        value = value.replace('!',
+                              '') if '!' in value else f'!{value}' if flag else value if '!' in value else \
+            value.replace(
+                '!', '')
         return value
 
     def sum(self, res, indexes):
@@ -245,13 +256,17 @@ class Karnough:
         res = self.create(*sorted(self.vars))
         indexes = []
         existing = []
+        indexes, existing = self.proces_lines(res, indexes, existing)
+        values = self.sum(res, indexes)
+        values = [i for i in values if i]
+        self.result_value = self.operation_out_constituents.join(values)
+
+    def proces_lines(self, res, indexes, existing):
         indexes, existing = self.check_line(res, indexes, existing)
         indexes, existing = self.check_four_figure(res, indexes, existing)
         indexes, existing = self.two_line(res, indexes, existing)
         indexes, existing = self.one_line(res, indexes, existing)
-        values = self.sum(res, indexes)
-        values = [i for i in values if i]
-        self.result_value = self.operation_out_constituents.join(values)
+        return indexes, existing
 
     def get_result(self):
         res = self.create(*sorted(self.vars))
@@ -268,6 +283,9 @@ class Karnough:
         second_value = set(self.items(second_value))
         combination = first_value.union(second_value)
         combination = set(map(lambda x: x.replace('!', ''), combination))
+        self.process_literals(combination, first_value, second_value)
+
+    def process_literals(self, combination, first_value, second_value):
         if len(combination) == len(first_value):
             new_literals = first_value.intersection(second_value)
             if len(new_literals) != len(first_value) - 1:
